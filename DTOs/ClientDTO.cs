49b789c42a003bd058846fef1c9f158706c68a39
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-02-05 16:37:27 -0300

    chore: validating dto

--- DTOs/ClientDTO.cs
+++ DTOs/ClientDTO.cs
@@ -1,13 +1,24 @@
+using System.ComponentModel.DataAnnotations;
 using Sellix.Entities.Enums;
 
 namespace Sellix.DTOs
 {
 	public class ClientDTO
 	{
+		[Required]
 		public string Name { get; set; } = string.Empty;
+
+		[Required]
+		[EmailAddress]
 		public string Email { get; set; } = string.Empty;
+
+		[Required]
 		public string Phone { get; set; } = string.Empty;
+
+		[Required]
 		public string Address { get; set; } = string.Empty;
+
+		[Required]
 		public ClientType Type { get; set; }
 	}
 }
