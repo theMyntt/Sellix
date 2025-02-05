2cff7188b5c9a52991a2c906156b4666e65decf8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-02-04 22:53:29 -0300

    chore: setting up jwt bearer token auth

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Sellix.Abstractions;
 using Sellix.Context;
 using Sellix.Repositories;
@@ -11,6 +14,29 @@ namespace Sellix
 		public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
 		{
 			var connectionString = configuration["MySql:ConnectionString"] ?? throw new Exception("MySql:ConnectionString Is Null");
+			var jwtSection = configuration.GetSection("Jwt");
+			var key = jwtSection["SecretKey"] ?? throw new Exception("SecretKey Is Null");
+			var keyBytes = Encoding.UTF8.GetBytes(key);
+
+			var issuer = jwtSection["Issuer"] ?? throw new Exception("Issuer Is Null");
+			var audience = jwtSection["Audience"] ?? throw new Exception("Audience Is Null");
+
+			services
+				.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters = new()
+					{
+						ValidateIssuer = true,
+						ValidateAudience = true,
+						ValidateLifetime = true,
+						ValidateIssuerSigningKey = true,
+						ValidIssuer = issuer,
+						ValidAudience = audience,
+						IssuerSigningKey = new SymmetricSecurityKey(keyBytes)
+					};
+				});
+			services.AddAuthorization();
 
 			services.AddDbContext<DatabaseContext>(options =>
 			{
@@ -24,6 +50,9 @@ namespace Sellix
 
 		public static IApplicationBuilder UseServices(this IApplicationBuilder app)
 		{
+			app.UseAuthentication();
+			app.UseAuthorization();
+
 			return app;
 		}
 	}
