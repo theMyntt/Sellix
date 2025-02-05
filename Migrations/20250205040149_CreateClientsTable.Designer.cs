4da24c23ca25e011379ef261e6beb1c802e93f9f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-02-05 01:02:54 -0300

    ci: create clients migration

--- Migrations/20250205040149_CreateClientsTable.Designer.cs
+++ Migrations/20250205040149_CreateClientsTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sellix.Context;
 
@@ -10,9 +11,11 @@ using Sellix.Context;
 namespace Sellix.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250205040149_CreateClientsTable")]
+    partial class CreateClientsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
