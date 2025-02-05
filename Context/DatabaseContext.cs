92947e836da2c61b623e95a827b8b206bc0e4577
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-02-05 01:01:01 -0300

    chore: add clients to db context

--- Context/DatabaseContext.cs
+++ Context/DatabaseContext.cs
@@ -7,6 +7,7 @@ namespace Sellix.Context
 	public class DatabaseContext : DbContext
 	{
 		public DbSet<UserEntity> Users { get; set; }
+		public DbSet<ClientEntity> Clients { get; set; }
 
 		public DatabaseContext(DbContextOptions options) : base(options)
 		{
