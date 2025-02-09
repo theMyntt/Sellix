2507ff207708b2560255f11cd9c47626243c5d55
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-02-09 01:46:46 -0300

    chore: foreign key configuration

--- Context/DatabaseContext.cs
+++ Context/DatabaseContext.cs
@@ -8,9 +8,25 @@ namespace Sellix.Context
 	{
 		public DbSet<UserEntity> Users { get; set; }
 		public DbSet<ClientEntity> Clients { get; set; }
+		public DbSet<SaleEntity> Sales { get; set; }
 
 		public DatabaseContext(DbContextOptions options) : base(options)
 		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<SaleEntity>()
+				.HasOne(u => u.Client)
+				.WithMany()
+				.HasForeignKey(u => u.ClientId);
+
+			modelBuilder.Entity<SaleEntity>()
+				.HasOne(u => u.User)
+				.WithMany()
+				.HasForeignKey(u => u.UserId);
+
+			base.OnModelCreating(modelBuilder);
 		}
 	}
 }
